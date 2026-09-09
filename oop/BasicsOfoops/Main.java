6a92fa7f9c4d1e2c0c9d8120a0090bd0b417739a
rati <patilratikant8@gmail.com>
2026-09-09 17:20:15 +0530

    Update code

---
+++ oop/BasicsOfoops/Main.java
@@ -0,0 +1,60 @@
+package BasicsOfoops;
+
+import java.awt.geom.Arc2D.Float;
+
+class Main {
+	 public static void main(String [] args) {
+		 
+		 
+		 int a  = 19; 
+		 int b = 12;
+		 
+		 
+		 add a1 = new add ();
+		 
+		 System.out.println(a1.addition(a, b));
+		 
+	 }
+
+}
+ 
+ 
+ class add {
+	 
+	 public int addition(int n , int m ) {
+		 
+		 int result = n + m ; 
+		 
+		 return result ;
+	 }
+	 
+	 		
+	 
+ }
+ 
+ 
+ class multiplication{
+	 
+public int addition(int n , int m ) {
+		 
+		 int result = n * m ; 
+		 
+		 return result ;
+	 }
+	 
+ }
+
+ 
+ 
+ class division {
+public double addition(int n , int m ) {
+		 
+		 float result = n / m ; 
+		 
+		 return result ;
+	 }
+	 
+	 
+	 
+	 
+ }
---
+++ oop/BasicsOfoops/Ratikant.java
@@ -0,0 +1,37 @@
+package BasicsOfoops;
+
+
+
+
+class A {
+	public static void main(String [] data) {
+		
+		System.out.println("A main method ");
+		
+	}
+
+}
+
+
+class b {
+	public static void main(String [] data) {
+		System.out.println("B in method ");
+		
+	}
+
+}
+
+
+class c {
+	public static void main(String [] data) {
+		System.out.println("C main method ");
+	}
+
+}
+
+
+class d {
+	
+
+}
+
