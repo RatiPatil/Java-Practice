747fe3903a5a051d294c594156d1457b8c2e3342
rati <patilratikant8@gmail.com>
2026-09-09 21:58:52 +0530

    Update code

---
+++ oop/BasicsOfoops/ImportStament.java
@@ -0,0 +1,26 @@
+package BasicsOfoops;
+
+import java.util.ArrayList;
+
+
+
+
+
+public class ImportStament {
+	
+	public static void main(String [] args) {
+		
+		ArrayList<Integer> l = new ArrayList<>();
+		
+		
+		
+		l.add(68);
+		l.add(79);
+		
+		
+		for(int num : l) {
+			System.out.println(num);
+		}
+	}
+
+}
