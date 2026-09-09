06860808d3407e0699076e2653d3e9ca7487caf1
rati <patilratikant8@gmail.com>
2026-09-09 17:32:25 +0530

    Update code

--- oop/BasicsOfoops/Ratikant.java
+++ oop/BasicsOfoops/Ratikant.java
@@ -1,37 +1,28 @@
 package BasicsOfoops;
 
-
-
+// If one class is public in this program, you must give that class name as the file name.
+// If no class is public in this program, you can give any name to the file.
+// Every class in this program will create its own separate .class file upon compilation, regardless of whether it has a main method.
+// If the program contains a public class, the file name must match that public class name.
 
 class A {
-	public static void main(String [] data) {
-		
-		System.out.println("A main method ");
-		
-	}
-
+    public static void main(String[] data) {
+        System.out.println("A main method");
+    }
 }
-
 
 class b {
-	public static void main(String [] data) {
-		System.out.println("B in method ");
-		
-	}
-
+    public static void main(String[] data) {
+        System.out.println("B in method");
+    }
 }
-
 
 class c {
-	public static void main(String [] data) {
-		System.out.println("C main method ");
-	}
-
+    public static void main(String[] data) {
+        System.out.println("C main method");
+    }
 }
-
 
 class d {
-	
-
+    // Empty class
 }
-
