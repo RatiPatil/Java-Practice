d415450df520c7fbf88cf718b3442d3d8f924940
rati <patilratikant8@gmail.com>
2026-09-10 00:26:58 +0530

    Update code

---
+++ oop/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import student.Student;
+
+public class Main {
+	
+	public static void main(String [] data) {
+		
+		Student s = new Student();
+		
+		s.DisplayDetails();
+	}
+
+}
---
+++ oop/student/Student.java
@@ -0,0 +1,17 @@
+package student;
+
+public class Student {
+	
+	String name =  "RatikantPatil";
+	int age  = 23 ;
+	String Coruse = "MCA";
+	
+	
+	public void DisplayDetails() {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(Coruse);
+		
+	}
+
+}
